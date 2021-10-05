d7192e98c1ca58553b85824e33a6ae2b4d7ac242
Naren <72463822+rnarendranath@users.noreply.github.com>
2021-10-05 16:56:00 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
 var http = require('http'); 
+var fs = require('fs');
+
+
 const PORT= process.env.PORT||5000;
 var server = http.createServer(function (req, res) {  
     if (req.url == '/') { 
-        
+            res.writeHead(200,{'Content-type':'text/html'});
+            
+            fs.readFile('./index.html',null,function(error,data){
 
-        res.writeHead(200, { 'Content-Type': 'text/html' }); 
-
-        res.write('<html><body><p>This is home Page.</p></body></html>');
-        res.end();
-    
-    }
-    else if (req.url == "/student") {
+                if(error){
+                    res.writeHead(404);
+                    res.write('File not Found!')
+                }else{
+                    res.write(data);
         
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is student Page.</p></body></html>');
+                }
         res.end();
+            });
     }
-    else if (req.url == "/admin") {
-        
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is admin Page.</p></body></html>');
-        res.end();
     
+    else if (req.url == "/successful.html") {
+        res.writeHead(200,{'Content-type':'text/html'});
+        fs.readFile('./successful.html',null,function(error,data){
+            if(error){
+                res.writeHead(404);
+                res.write('File not Found!')
+            }else{
+                res.write(data);
+    
+            }
+    res.end();
+        });
     }
+  
     else
     
         res.end('Invalid Request!');
